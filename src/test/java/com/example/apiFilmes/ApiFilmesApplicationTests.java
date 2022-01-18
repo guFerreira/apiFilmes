403166ef8b1459aef26913b8c851f5ce9c36caea
Gustavo <gustavo.soares.ferreira@hotmail.com>
2022-01-18 11:38:10 -0300

    Criação do Projeto Spring Boot

---
+++ src/test/java/com/example/apiFilmes/ApiFilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiFilmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
