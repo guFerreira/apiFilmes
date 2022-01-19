dfab5b48b73e802e75e12e6370e30e6c4cbcf82e
Gustavo <gustavo.soares.ferreira@hotmail.com>
2022-01-18 22:36:38 -0300

    Criando query JPQL para retornar a média das avaliações de um filme

--- src/main/java/com/example/apiFilmes/repository/AvaliacaoRepository.java
+++ src/main/java/com/example/apiFilmes/repository/AvaliacaoRepository.java
@@ -2,6 +2,12 @@ package com.example.apiFilmes.repository;
 
 import com.example.apiFilmes.model.Avaliacao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AvaliacaoRepository extends JpaRepository<Avaliacao,Long> {
+    @Query(value = "SELECT coalesce(AVG(avaliacao.nota),0) FROM Avaliacao avaliacao WHERE avaliacao.filme.id = :idFilme")
+    public float getMediaAvaliacaoByFilmeId(@Param("idFilme") Long idFilme);
+
+    //SELECT filmes FROM Filme filme JOIN Avaliacao avaliacao WHERE avaliacao.filme.id = filme.id and
 }
